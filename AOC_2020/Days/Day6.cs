7f8d6312a70918e0188efa4db0eef37b6af060bf
sk0gen <gawinskiwojciech@gmail.com>
2020-12-06 11:48:21 +0100

    Part two of day6

--- AOC_2020/Days/Day6.cs
+++ AOC_2020/Days/Day6.cs
@@ -1,12 +1,12 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AOC_2020.Days
 {
 
     public class Day6 : SeparatedDay
     {
-        private string[] GroupFormData;
+        private string[] GroupsData;
 
         protected override object part1()
         {
@@ -15,37 +15,70 @@ namespace AOC_2020.Days
 
         protected override object part2()
         {
-            return null;
+            return CountGroupsWhereEveryoneAnswersYesToSameQuestions();
         }
 
         private int CountGroupsYesAnswers()
         {
             int count = default;
-            for (var i = 0; i < GroupFormData.Length; ++i)
+            for (var i = 0; i < GroupsData.Length; ++i)
             {
-                count += CountYesInGroup(GroupFormData[i]);
+                count += CountYesInGroup(GroupsData[i]);
             }
 
-
             return count;
+
             int CountYesInGroup(string groupData)
             {
-                var yesAnswers = new HashSet<char>(64);
+                var yesAnswers = new HashSet<char>(26);
                 foreach (var yesAnswer in groupData)
                 {
                     yesAnswers.Add(yesAnswer);
                 }
-                
+
                 yesAnswers.Remove('\n');
                 return yesAnswers.Count;
             }
-            
+        }
+
+        private int CountGroupsWhereEveryoneAnswersYesToSameQuestions()
+        {
+            var groupSize = 0;
+            var total = 0;
+            var groupAnswers = new int[26];
+            for (int i = 0; i < Lines.Count; ++i)
+            {
+                if (Lines[i] != "")
+                {
+                    foreach (var answer in Lines[i])
+                    {
+                        groupAnswers[answer - 97] += 1;
+                    }
+                    groupSize++;
+                }
+                else
+                {
+                    for (var index = 0; index < groupAnswers.Length; index++)
+                    {
+                        var answer = groupAnswers[index];
+                        if (answer == groupSize)
+                        {
+                            total++;
+                        }
+                    }
+
+                    groupAnswers = new int[26];
+                    groupSize = 0;
+                }
+            }
+
+            return total;
         }
 
         protected override void Parse()
         {
             var fileString = string.Join("\n", Lines.ToArray());
-            GroupFormData = fileString.Split("\n\n");
+            GroupsData = fileString.Split("\n\n");
         }
     }
 
